4952ec207ac731e919df28341d32e3372af6b049
Petr Romantsov <romantsovp@gmail.com>
2025-06-13 13:56:27 +0300

    update NavLink/add fontsize to props

--- src/components/NavLink/NavLink.jsx
+++ src/components/NavLink/NavLink.jsx
@@ -1,9 +1,13 @@
 import styles from './NavLink.module.css';
 
-export const NavLink = ({ children, link }) => {
+export const NavLink = ({ children, link, size }) => {
+  const linkClassName = `nav__link--${size}`;
+
   return (
     <li className={styles.nav__item}>
-      <a href={link}>{children}</a>
+      <a className={linkClassName} href={link}>
+        {children}
+      </a>
     </li>
   );
 };
--- src/components/NavList/NavList.jsx
+++ src/components/NavList/NavList.jsx
@@ -3,9 +3,15 @@ import { NavLink } from '../navLink/navLink';
 export const NavList = () => {
   return (
     <ul>
-      <NavLink link={'#conception'}>О КОЛЛАБОРАЦИИ</NavLink>
-      <NavLink link={'#series'}>серия Mi 10T</NavLink>
-      <NavLink link={'#wherebuy'}>ГДЕ КУПИТЬ</NavLink>
+      <NavLink link={'#conception'} size={'sm'}>
+        О КОЛЛАБОРАЦИИ
+      </NavLink>
+      <NavLink link={'#series'} size={'sm'}>
+        серия Mi 10T
+      </NavLink>
+      <NavLink link={'#wherebuy'} size={'sm'}>
+        ГДЕ КУПИТЬ
+      </NavLink>
     </ul>
   );
 };
